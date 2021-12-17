bd5ec47f5a5895ae4b698c88a5a907ed74f3f930
LuisBorges7 <luisborges.contato@gmail.com>
2021-12-17 12:26:36 -0300

    Config settings.py Static Root e Staticfiles_Dirs

--- projetoFinal/backend/carRent/settings.py
+++ projetoFinal/backend/carRent/settings.py
@@ -145,7 +145,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = "app-root/repo/wsgi/static"
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('assets', 'app-root/repo/wsgi/openshift/static'),
+
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
