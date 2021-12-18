ca962612ade0a320431d1a56446edb438975b22d
LuisBorges7 <luisborges.contato@gmail.com>
2021-12-18 19:47:11 -0300

    Allowed hosts pweb-luxcar.herokuapp.com and 127.0.01 - Debug = False

--- projetoFinal/backend/carRent/settings.py
+++ projetoFinal/backend/carRent/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*93*s4f3@kj)ie(_v!n4i4$k(y#x^+&no=fwjbxb^!vlee_4lr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pweb-luxcar.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
